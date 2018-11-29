f2b8d8347f331b906191f2580499fde7d6430a8c
Arjun G <arjupba@gmail.com>
2018-11-29 23:33:34 +0530

    Bug Fix

--- dist/index.js
+++ dist/index.js
@@ -1,30 +1,38 @@
 var _lodash = require("lodash");
 var objectID = require("mongodb").ObjectID;
 
-export const idValidate = (schema = {}, key = Object.keys(schema)[0]) => (
-  req,
-  res,
-  next
-) => {
-  _lodash.isEmpty(schema)
-    ? next()
-    : req.body[key]
-      ? objectID.isValid(req.body[key])
-        ? next()
-        : res.status(400).json({
-            name: "invalid",
-            param: key,
-            required: true,
-            valid: false,
-            message: key + " is invalid"
-          })
-      : schema[key].required
-        ? res.status(400).json({
-            name: "required",
-            param: key,
-            required: true,
-            valid: false,
-            message: key + " is required"
-          })
-        : next();
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.idValidate = idValidate;
+
+function idValidate(schema = {}, key = Object.keys(schema)[0]) {
+  return function(req, res, next) {
+    _lodash.isEmpty(schema)
+      ? next()
+      : req.body[key]
+        ? objectID.isValid(req.body[key])
+          ? next()
+          : res.status(400).json({
+              name: "invalid",
+              param: key,
+              required: true,
+              valid: false,
+              message: key + " is invalid"
+            })
+        : schema[key].required
+          ? res.status(400).json({
+              name: "required",
+              param: key,
+              required: true,
+              valid: false,
+              message: key + " is required"
+            })
+          : next();
+  };
+}
+
+exports.default = {
+  idValidate: idValidate
 };
