cfe3389b9679df0c6a05ab0774f745eb9f8895be
Arjun G <arjupba@gmail.com>
2020-03-29 13:40:26 +0530

    id validate skip for some string

--- README.md
+++ README.md
@@ -14,6 +14,20 @@ import { idValidate, idValidateInParam } from "objectid-validator";
 router.post("/create/", idValidate({ id: { required: true } }), create);
 // This will return Network status code 400 with error messages if "id" in "req.body" is not valid
 
-router.get('/getData/:id', token({ required: true }), idValidateInParam('id'), getData)
+router.get(
+  "/getData/:id",
+  token({ required: true }),
+  idValidateInParam("id"),
+  getData
+);
 // This will return Network status code 400 with error messages if "id" in "req.params" is not valid
+
+router.get(
+  "/getData/:id",
+  token({ required: true }),
+  idValidateInParam("id", ["me"]),
+  getData
+);
+// This will return Network status code 400 with error messages if "id" in "req.params" is not valid
+// And it will not validate "id" if we passed string "me" instead of an ObjectId
 ```
--- dist/index.js
+++ dist/index.js
@@ -1,64 +1,69 @@
-var _lodash = require('lodash')
-var objectID = require('mongoose').Types.ObjectId
+var _lodash = require("lodash");
+var objectID = require("mongoose").Types.ObjectId;
 
-Object.defineProperty(exports, '__esModule', {
+Object.defineProperty(exports, "__esModule", {
   value: true
-})
+});
 
-exports.idValidate = idValidate
-exports.idValidateInParam = idValidateInParam
+exports.idValidate = idValidate;
+exports.idValidateInParam = idValidateInParam;
 
-function idValidate (schema = {}, key = Object.keys(schema)[0]) {
-  return function (req, res, next) {
+function idValidate(schema = {}, key = Object.keys(schema)[0]) {
+  return function(req, res, next) {
     _lodash.isEmpty(schema)
       ? next()
       : req.body[key]
-        ? objectID.isValid(req.body[key])
-          ? next()
-          : res.status(400).json({
-            name: 'invalid',
-            param: key,
-            required: true,
-            valid: false,
-            message: key + ' is invalid'
-          })
-        : schema[key].required
-          ? res.status(400).json({
-            name: 'required',
-            param: key,
-            required: true,
-            valid: false,
-            message: key + ' is required'
-          })
-          : next()
-  }
-}
-
-function idValidateInParam (key) {
-  return function (req, res, next) {
-    _lodash.isEmpty(key)
-      ? next()
-      : req.params[key]
-        ? objectID.isValid(req.params[key])
-          ? next()
-          : res.status(400).json({
-            name: 'invalid',
-            param: key,
-            required: true,
-            valid: false,
-            message: key + ' is invalid'
-          })
+      ? objectID.isValid(req.body[key])
+        ? next()
         : res.status(400).json({
-          name: 'required',
+            name: "invalid",
+            param: key,
+            required: true,
+            valid: false,
+            message: key + " is invalid"
+          })
+      : schema[key].required
+      ? res.status(400).json({
+          name: "required",
           param: key,
           required: true,
           valid: false,
-          message: key + ' is required'
+          message: key + " is required"
         })
+      : next();
+  };
+}
+
+function idValidateInParam(key, arr) {
+  if (!arr) {
+    arr = [];
   }
+  return function(req, res, next) {
+    _lodash.isEmpty(key)
+      ? next()
+      : req.params[key]
+      ? arr.indexOf(req.params[key]) === -1
+        ? objectID.isValid(req.params[key])
+          ? next()
+          : res.status(400).json({
+              name: "invalid",
+              param: key,
+              required: true,
+              valid: false,
+              message: key + " is invalid"
+            })
+        : next()
+      : res.status(400).json({
+          name: "required",
+          param: key,
+          required: true,
+          valid: false,
+          message: key + " is required"
+        });
+  };
 }
 
 exports.default = {
   idValidate: idValidate,
   idValidateInParam: idValidateInParam
-}
+};
