eb2145ff53e96dc7c9f658c7f7d289f1bce0ccc4
Arjun G <arjupba@gmail.com>
2024-04-14 13:39:40 +0530

    devops: ci cd pipeline enabled

---
+++ .github/config/config.js
@@ -0,0 +1,20 @@
+"use strict";
+const config = require("conventional-changelog-conventionalcommits");
+
+module.exports = config({
+  types: [
+    { type: "build", hidden: true }, // hidden
+    { type: "release", hidden: true }, // hidden
+    { type: "deprecate", section: "Deprecated" }, // minor
+    { type: "devops", hidden: true }, // hidden
+    { type: "docs", hidden: true }, // hidden
+    { type: "feat", section: "Added" }, // minor
+    { type: "fix", section: "Fixed" }, // patch
+    { type: "perf", section: "Performance" }, // patch
+    { type: "refactor", hidden: true }, // hidden
+    { type: "remove", section: "Removed" }, // minor
+    { type: "security", section: "Security" }, // patch
+    { type: "style", hidden: true }, // hidden
+    { type: "test", hidden: true }, // hidden
+  ],
+});
